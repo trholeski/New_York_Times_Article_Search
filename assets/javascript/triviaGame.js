25346bb86e0de045d378011a883b7c1078db055b
SilverBell17 <kevinjoun17@gmail.com>
2019-06-28 19:10:29 -0700

    added a small change to the html. Removed the onclick that was in the button. Added most of javascript code. Still need to debug and get it working.

--- assets/javascript/triviaGame.js
+++ assets/javascript/triviaGame.js
@@ -1,18 +1,40 @@
-var search = "#searchInput"
-var numberOfRecords = "#recordNum" 
-var startYear = "#startYear" + "0101"
-var endYear = "#endYear" + "1231"
+$(document).ready(function(){
+//defining some global variables that will be used as the placeholder of user input.
+var search 
+var numberOfRecords 
+var startYear
+var endYear
 var key = "mFOHoR2tOylWqAA7pUS0mjkNpgXdMziR"
-var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=" + key + "&q=" + search + "&begin_date=" + startYear + "&end_date=" + endYear
+var queryURL
 
-$("#submitSearch").on("click",function(){
-    search = $("#searchInput").attr("value");
-    numberOfRecords = $("#recordNum").attr("value");
-    startYear = $("#startYear").attr("value");
-    endYear = $("#endYear").attr("value");
-})
+//when the user clicks "submit query" button, the global variables defined above are redefined to the respective user input. queryURL is also redefined to be the full api url.
+$("#submitSearch").on("click", function(){
+    search = $("#searchInput").val();
+    numberOfRecords = $("#recordNum").val();
+    startYear = $("#startYear").val();
+    endYear = $("#endYear").val();
+    queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=" + key + "&q=" + search + "&begin_date=" + startYear + "0101&end_date=" + endYear + "1231"
+    console.log(queryURL);
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response) {
+        console.log(response);
+        var results = response.docs;
+        for(var i = 0; i<numberOfRecords; i++) {
+            var div = $("<div>");
+            var p = $("<p>");
+            var headline = p.text("Headline: " + results[i].headline.main);
+            var link = p.text("Link: " + results[i].web_url);
+            var abstract = p.text("Abstract: " + results[i].abstract);
+            div.append(headline);
+            div.append(link);
+            div.append(abstract);
+            $("#").prepend(div);
+        }
+    });
+});
 
-//search button id="submitSearch"
-//clear results button id="clearResults"
+});
 
-console.log(queryURL); //console logging for test of cantcatnation (don't spell check that).
+
